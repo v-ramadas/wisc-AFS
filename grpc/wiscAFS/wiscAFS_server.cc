ec125286e2d8cc26c6262cf77b4466b32da09522
VarunThfc <varun.kaundinya13@gmail.com>
2023-02-10 18:38:16 -0600

    server_open 1

--- grpc/wiscAFS/wiscAFS_server.cc
+++ grpc/wiscAFS/wiscAFS_server.cc
@@ -60,26 +60,26 @@ class wiscAFSImpl final : public AFSController::Service {
  }
 };
 
-//void RunServer() {
-//  std::string server_address("10.10.1.2:50051");
-//  GreeterServiceImpl service;
-//
-//  grpc::EnableDefaultHealthCheckService(true);
-//  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-//  ServerBuilder builder;
-//  // Listen on the given address without any authentication mechanism.
-//  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-//  // Register "service" as the instance through which we'll communicate with
-//  // clients. In this case it corresponds to an *synchronous* service.
-//  builder.RegisterService(&service);
-//  // Finally assemble the server.
-//  std::unique_ptr<Server> server(builder.BuildAndStart());
-//  std::cout << "Server listening on " << server_address << std::endl;
-//
-//  // Wait for the server to shutdown. Note that some other thread must be
-//  // responsible for shutting down the server for this call to ever return.
-//  server->Wait();
-//}
+void RunServer() {
+ std::string server_address("10.10.1.2:50051");
+ GreeterServiceImpl service;
+
+ grpc::EnableDefaultHealthCheckService(true);
+ grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+ ServerBuilder builder;
+ // Listen on the given address without any authentication mechanism.
+ builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+ // Register "service" as the instance through which we'll communicate with
+ // clients. In this case it corresponds to an *synchronous* service.
+ builder.RegisterService(&service);
+ // Finally assemble the server.
+ std::unique_ptr<Server> server(builder.BuildAndStart());
+ std::cout << "Server listening on " << server_address << std::endl;
+
+ // Wait for the server to shutdown. Note that some other thread must be
+ // responsible for shutting down the server for this call to ever return.
+ server->Wait();
+}
 
 int main(int argc, char** argv) {
 //  RunServer();
