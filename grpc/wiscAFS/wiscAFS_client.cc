49530b33937ce0d7dd59f570eb369be186e995a3
deep-mac <deepmachchhar@gmail.com>
2023-02-11 16:14:53 -0600

    wiscAFS client updates

--- grpc/wiscAFS/wiscAFS_client.cc
+++ grpc/wiscAFS/wiscAFS_client.cc
@@ -67,10 +67,204 @@ class wiscAFSClient {
     } else {
       std::cout << status.error_code() << ": " << status.error_message()
                 << std::endl;
-      //TODO : Return error instead
+      reply.set_status(-1);
       return reply;
     }
   }
+
+  RPCResponse CloseFile(const std::string& filename, const std::string& path, const string& data) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(filename);
+    request.set_path(path);
+    request.set_data(data);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->CloseFile(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse CreateFile(const std::string& filename, const std::string& path, const int mode) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(filename);
+    request.set_path(path);
+    request.set_mode(mode);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->CreateFile(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse DeleteFile(const std::string& filename, const std::string& path) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(filename);
+    request.set_path(path);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->DeleteFile(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse CreateDir(const std::string& dirname, const std::string& path, const int mode) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(filename);
+    request.set_path(path);
+    request.set_path(mode);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->CreateFile(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse OpenDir(const std::string& dirname, const std::string& path, const int mode) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(dirname);
+    request.set_path(path);
+    request.set_path(mode);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->OpenFile(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse RemoveDir(const std::string& dirname, const std::string& path) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(dirname);
+    request.set_path(path);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->RemoveDir(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
+  RPCResponse GetAttr(const std::string& filename, const std::string& path) {
+    // Data we are sending to the server.
+    RPCRequest request;
+    request.set_filename(filename);
+    request.set_path(path);
+
+    // Container for the data we expect from the server.
+    RPCResponse reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->GetAttr(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      reply.set_status(-1);
+      return reply;
+    }
+  }
+
 
  private:
   std::unique_ptr<AFSController::Stub> stub_;
@@ -107,10 +301,43 @@ int main(int argc, char** argv) {
   wiscAFSClient afsClient (
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   std::string filename("a.txt");
+  std::string dirname("dir1");
   std::string path("/users/vramadas/grpc/examples/cpp/wiscAFS/cmake/build/");
+  std::string data("asdafgdf\nssfdlkjgdgklfg\n");
+  std::string data("asdafgdf\nssfdlkjgdgklfg\n");
   int flags = O_RDONLY;
-  RPCResponse reply = afsClient.OpenFile(filename, path, flags);
+  int mode = S_IRWXU;
+  std::cout << "Sending CreateFile\n" ;
+  RPCResponse reply = afsClient.CreateFile(filename, path, mode);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
+
+  std::cout << "Sending OpenFile\n" ;
+  reply = afsClient.OpenFile(filename, path, flags);
   std::cout << "Data recieved : " << reply.data() << std::endl;
+
+  std::cout << "Sending GetAttr\n" ;
+  reply = afsClient.GetAttr(filename, path);
+  std::cout << "Data recieved : " << reply.RPCAttr().filesize() << std::endl;
+
+  std::cout << "Sending CloseFile\n" ;
+  reply = afsClient.CloseFile(filename, path, data);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
+
+  std::cout << "Sending DeleteFile\n" ;
+  reply = afsClient.DeleteFile(filename, path);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
+
+  std::cout << "Sending CreateDir\n" ;
+  reply = afsClient.CreateDir(dirname, path);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
+
+  std::cout << "Sending OpenDir\n" ;
+  reply = afsClient.OpenDir(dirname, path);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
+
+  std::cout << "Sending RemoveDir\n" ;
+  reply = afsClient.RemoveDir(dirname, path);
+  std::cout << "Response recieved : " << reply.status() << std::endl;
 
   return 0;
 }
