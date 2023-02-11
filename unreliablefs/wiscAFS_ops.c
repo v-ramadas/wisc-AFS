2f8515d01784f6bedd322ce63c725d138fc813bc
vramadas <vramadas@node0.group7.advosuwmadison-pg0.wisc.cloudlab.us>
2023-02-11 16:14:01 -0600

    remove readdir from unreliablefs

--- unreliablefs/wiscAFS_ops.c
+++ unreliablefs/wiscAFS_ops.c
@@ -136,24 +136,25 @@ int wiscAFS_opendir(const char *path, struct fuse_file_info *fi)
 int wiscAFS_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                        off_t offset, struct fuse_file_info *fi)
 {
-    DIR *dp = afsClient.OpenDir(path);
-    if (dp == NULL) {
-	return -errno;
-    }
-    struct dirent *de;
-
-    (void) offset;
-    (void) fi;
-
-    while ((de = afsClient.ReadDir(dp)) != NULL) {
-        struct stat st;
-        memset(&st, 0, sizeof(st));
-        st.st_ino = de->d_ino;
-        st.st_mode = de->d_type << 12;
-        if (filler(buf, de->d_name, &st, 0))
-            break;
-    }
-    closedir(dp);
+//    TODO : Implement later if needed
+//    DIR *dp = afsClient.OpenDir(path);
+//    if (dp == NULL) {
+//	return -errno;
+//    }
+//    struct dirent *de;
+//
+//    (void) offset;
+//    (void) fi;
+//
+//    while ((de = afsClient.ReadDir(dp)) != NULL) {
+//        struct stat st;
+//        memset(&st, 0, sizeof(st));
+//        st.st_ino = de->d_ino;
+//        st.st_mode = de->d_type << 12;
+//        if (filler(buf, de->d_name, &st, 0))
+//            break;
+//    }
+//    closedir(dp);
 
     return 0;
 }
